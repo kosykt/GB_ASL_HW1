85bf61ebffb0ee32d0ec7728f8ae2c666cd92841
kos_ykt <kosvasl09@gamil.com>
2021-06-06 20:24:10 +0900

    test3

--- app/src/main/java/com/example/gb_asl_hw1/MainActivity.java
+++ app/src/main/java/com/example/gb_asl_hw1/MainActivity.java
@@ -18,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
 //        test2
 //        test2
 //        test3
+//        test3
+//        test3
+//        test3
+//        test3
     }
 }
